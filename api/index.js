59f794febc7c6e81b6e9e9d0353d83281450988c
Mohsin Pannu <mohsin.pannu170@gmail.com>
2023-11-19 07:58:38 +0500

    added dotenv

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const port = process.env.PORT
 
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
-  res.json('You are at the home page/ Api is working')
+  res.send('Hello World')
 })
 
-app.listen(port, () => console.log(`Api is running on port ${3000}`))
+app.listen(port, () => console.log(`Api is running on port ${port}`))
